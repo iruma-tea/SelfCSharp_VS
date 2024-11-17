f2c170599548d057d735f6500135f476847b1c5b
Shusuke Nakamura <shusuke.pana@gmail.com>
2024-11-17 19:04:27 +0900

    リスト2.2の学習完了

---
+++ Chap02/StringVariable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SelfCSharp.Chap02
+{
+    internal class StringVariable
+    {
+        static void Main(string[] args)
+        {
+            string name = "山田";
+            Console.WriteLine($"こんにちは、{name}さん！");
+        }
+    }
+}
