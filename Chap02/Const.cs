0679118edd025bc5fd2665e2762e7dec4be98781
Shusuke Nakamura <shusuke.pana@gmail.com>
2024-11-17 18:55:56 +0900

    リスト2.1まで学習完了

---
+++ Chap02/Const.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SelfCSharp.Chap02
+{
+    internal class Const
+    {
+        static void Main(string[] args)
+        {
+            const double Tax = 1.1;
+            int price = 1000;
+            double sum = price * Tax;
+            Console.WriteLine(sum);
+        }
+    }
+}
