04dcf760cd4c6c50ac6728f30de3f90f3ab3651f
Shusuke Nakamura <shusuke.pana@gmail.com>
2024-12-01 08:47:05 +0900

    リスト5-14まで学習完了

---
+++ Chap05/StrFor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SelfCSharp.Chap05
+{
+    internal class StrFor
+    {
+        static void Main(string[] args)
+        {
+            var str = "ウイングス";
+            foreach(var s in str)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
