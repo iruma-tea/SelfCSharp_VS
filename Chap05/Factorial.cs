e0371429e915076832e04eded8042564859eb2ca
Shusuke Nakamura <shusuke.pana@gmail.com>
2024-12-25 23:34:45 +0900

    リスト5.60まで学習完了

---
+++ Chap05/Factorial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SelfCSharp.Chap05
+{
+    internal class Factorial
+    {
+        static void Main(string[] args)
+        {
+            long result = 1;
+            for (var i = 1; i < 26; i++)
+            {
+                // 1 ～ 25の値を順に乗算
+                result *= i;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
