19e607e54cf5223637582f10607ca2e0673cfd61
iruma-tea <shusuke.think.pad@gmail.com>
2024-12-30 21:03:18 +0900

    Chap05の学習完了

---
+++ Chap06/practice/Practice2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SelfCSharp.Chap06.practice
+{
+    internal class Practice2
+    {
+        static void Main(string[] args)
+        {
+            var dic = new Dictionary<string, string>()
+            {
+                ["lettuce"] = "レタス",
+                ["spinach"] = "ホウレンソウ",
+                ["cucumber"] = "キュウリ"
+            };
+            dic["cucumber"] = "キュウリ";
+            dic.Add("cabbage", "キャベツ");
+            dic.Remove("spinach");
+            foreach (var m in dic)
+            {
+                Console.WriteLine($"{m.Key} : {m.Value}");
+            }
+        }
+    }
+}
---
+++ Chap06/practice/Practice3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SelfCSharp.Chap06.practice
+{
+    internal class Practice3
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>() { 1, 2, 3, 4 };
+            list[2] = 50;
+            list.Insert(1, 5);
+            list.Remove(60);
+            foreach (var v in list)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
