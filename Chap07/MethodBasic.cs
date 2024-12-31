cbff7f0eb4949a04f2e35d282409845037a7dc9e
iruma-tea <shusuke.think.pad@gmail.com>
2024-12-31 10:52:20 +0900

    リスト7-10まで学習完了

---
+++ Chap07/MethodBasic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SelfCSharp.Chap07.ClassMethod
+{
+    internal class Person
+    {
+        public string firstName = "";
+        public string lastName = "";
+
+        public string Show()
+        {
+            return $"名前は{this.lastName}{this.firstName}";
+        }
+
+        //戻り値がない場合
+        //public void Show()
+        //{
+        //    Console.WriteLine($"名前は{this.lastName}{this.firstName}");
+        //}
+
+        //何もしない
+        //public void Show()
+        //{
+        //    Console.WriteLine($"名前は{this.lastName} {this.firstName}");
+        //    return;
+        //}
+    }
+    internal class MethodBasic
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+            p.firstName = "太郎";
+            p.lastName = "山田";
+            Console.WriteLine(p.Show());
+        }
+    }
+}
