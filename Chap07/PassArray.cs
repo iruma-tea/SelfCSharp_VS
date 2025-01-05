631113dc05c73b90adc5bc633930f263d2b14651
Shusuke Nakamura <shusuke.pana@gmail.com>
2025-01-05 14:59:17 +0900

    リスト7.45まで学習完了

---
+++ Chap07/PassArray.cs
@@ -0,0 +1,19 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class PassArray
+    {
+        public int[] Update(int[] data)
+        {
+            //data[0] = 5;
+            data = new[] { 10, 20, 30 };
+            return data;
+        }
+        static void Main(string[] args)
+        {
+            var data = new[] { 2, 4, 6 };
+            var p = new PassArray();
+            Console.WriteLine(p.Update(data)[0]);
+            Console.WriteLine(data[0]);
+        }
+    }
+}
---
+++ Chap07/PassRefArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SelfCSharp.Chap07
+{
+    internal class PassRefArray
+    {
+        public int[] Update(ref int[] data)
+        {
+            //data[0] = 5;
+            data = new[] { 10, 20, 30 };
+            return data;
+        }
+        static void Main(string[] args)
+        {
+            var data = new[] { 2, 4, 6 };
+            var p = new PassRefArray();
+            Console.WriteLine(p.Update(ref data)[0]);
+            Console.WriteLine(data[0]);
+        }
+    }
+}
