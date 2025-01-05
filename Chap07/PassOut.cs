864ef46f26a00840f91d8d89e9eaf09d2f522bc8
Shusuke Nakamura <shusuke.pana@gmail.com>
2025-01-05 16:29:21 +0900

    リスト7.54まで学習完了

---
+++ Chap07/PassOut.cs
@@ -0,0 +1,28 @@
+﻿namespace SelfCSharp.Chap07
+{
+    internal class PassOut
+    {
+        public void GetMaxMin(int x, int y, out int max, out int min)
+        {
+            if (x >= y)
+            {
+                max = x;
+                min = y;
+            }
+            else
+            {
+                max = y;
+                min = x;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var a = new PassOut();
+            int resutMax, resultMin;
+            a.GetMaxMin(5, 2, out resutMax, out resultMin);
+            Console.WriteLine(resutMax);
+            Console.WriteLine(resultMin);
+        }
+    }
+}
