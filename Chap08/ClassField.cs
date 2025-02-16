bbc7b8d8aa3302c39d4d4f9491cc342108b9bbe6
iruma-tea <shusuke.think.pad@gmail.com>
2025-02-16 19:37:33 +0900

    Chap08の学習開始

---
+++ Chap08/ClassField.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SelfCSharp.Chap08.PublicField
+{
+    internal class Triangle
+    {
+        public double width; // 底辺
+        public double height; // 高さ
+
+        public double GetArea()
+        {
+            return this.width * this.height / 2;
+        }
+    }
+    internal class ClassField
+    {
+        static void Main(string[] args)
+        {
+            var t = new Triangle();
+            t.width = 10;
+            t.height = 5;
+            Console.WriteLine(t.GetArea());
+        }
+    }
+}
